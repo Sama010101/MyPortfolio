552cc1bc84bbfc557f1979a1e2fa4e0a0787b95d
Sama010101 <samucaferreira02658@gmail.com>
2025-09-04 18:53:01 -0300

    Add novos arquivos

---
+++ src/utils/HeadginHero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const HeadingHero = () => {
+    const roles = [
+        {
+            title: "Desenvolvedor Frontend &  DevOps"
+        }
+    ]
+  return (
+    <div className="text-center">
+    </div>
+  );
+}
